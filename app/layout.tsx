60423737aeb8401f94090d1a26c7c4587d8a3d0a
Masakun <kmkn0419@gmail.com>
2025-05-24 03:49:40 +0900

    any+

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import './globals.css'
+import { ReactNode } from 'react'
 
 export const metadata = {
   title: 'アイテム検索',
   description: 'JSONでアイテムを検索できるアプリ',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <body>{children}</body>
